fc3f000635c792e2b2d0aaf0bc81fe555457fb92
maggievega <maggievega@hotmail.com>
2015-05-14 16:47:02 -0300

    Assets

--- src/ar/edu/itba/Asteroids/Core/Assets.java
+++ src/ar/edu/itba/Asteroids/Core/Assets.java
@@ -4,5 +4,14 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class Assets {
 
+	//Asteroides
 	public static final Texture ASTEROID = new Texture("aster.png");
+	
+	//Naves
+	public static final Texture[] NAVES = {new Texture("death.png"),new Texture("capsule.png"),new Texture("nose.png"),new Texture("rosca.png"),new Texture("ufo.png")};
+	
+	//PowerUps
+	public static final Texture EXTRAVIDAIMG = new Texture("extravida.png");
+	public static final Texture EXTRAMASAIMG = new Texture("extramasa.png");
+	public static final Texture INVENCIBILIDADIMG = new Texture("invencibilidad.png");
 }
